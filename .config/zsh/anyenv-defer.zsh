ce5e0b2147cec0063c58a7f7273b1df7a9669874
nekottyo <nekottyo@gmail.com>
2024-05-08 19:17:22 +0900

    feat: speed up

--- .config/zsh/anyenv-defer.zsh
+++ .config/zsh/anyenv-defer.zsh
@@ -5,4 +5,20 @@
 
 exists "npm" && export PATH=${PATH}:$(npm bin):$(npm -g  bin)
 
-eval "$(anyenv init --no-rehash -)"
+# https://zenn.dev/ktakayama/articles/27b9d6218ed2f0ee9992
+if exists "anyenv"
+then
+   if ! [ -f /tmp/anyenv.cache ]
+   then
+      anyenv init - --no-rehash > /tmp/anyenv.cache
+      zcompile /tmp/anyenv.cache
+   fi
+   source /tmp/anyenv.cache
+
+   if ! [ -f /tmp/nodeenv.cache ]
+   then
+      nodenv init - > /tmp/nodeenv.cache
+      zcompile /tmp/nodeenv.cache
+   fi
+   source /tmp/nodeenv.cache
+fi
