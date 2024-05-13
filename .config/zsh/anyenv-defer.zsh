17def5a1184c2aff3c200ea590d68a1f437c5c0c
nekottyo <nekottyo@gmail.com>
2024-05-13 15:14:39 +0900

    fix: update

--- .config/zsh/anyenv-defer.zsh
+++ .config/zsh/anyenv-defer.zsh
@@ -3,22 +3,22 @@
 # export GOROOT=$(go env GOROOT )
 # export PATH=${GOPATH}/bin:$(go env GOPATH)/bin:${PATH}
 
-exists "npm" && export PATH=${PATH}:$(npm bin):$(npm -g  bin)
+source ~/.config/zsh/functions/exists
 
 # https://zenn.dev/ktakayama/articles/27b9d6218ed2f0ee9992
 if exists "anyenv"
 then
-   if ! [ -f /tmp/anyenv.cache ]
-   then
+   if ! [ -f /tmp/anyenv.cache ]; then
       anyenv init - --no-rehash > /tmp/anyenv.cache
       zcompile /tmp/anyenv.cache
    fi
    source /tmp/anyenv.cache
 
-   if ! [ -f /tmp/nodeenv.cache ]
-   then
+   if ! [ -f /tmp/nodeenv.cache ]; then
       nodenv init - > /tmp/nodeenv.cache
       zcompile /tmp/nodeenv.cache
    fi
    source /tmp/nodeenv.cache
 fi
+
+exists "npm" && export PATH=${PATH}:$(npm bin):$(npm -g  bin)
